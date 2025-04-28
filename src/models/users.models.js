ca3ad83f0f6ed7684b44026ea4c4b45f127766a9
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-04-28 21:34:31 +0530

    add password hashing using bcrypt pre-save hook

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -51,4 +51,19 @@ const userSchema =new mongoose.Schema(
     {timestamps:true});
 
 
+//password hashing using bcrypt pre-save hook
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+
+    this.password =await bcrypt.hash(this.password,10);
+    next();
+    
+//iscorrect password
+userSchema.methods.isCorrectPassword = async function (password){
+    return await bcrypt.compare(password,this.password)
+
+}
+
+})
+
 export const User =mongoose.model("User",userSchema)
