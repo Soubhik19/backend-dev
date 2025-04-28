89a1279aa88e0a004aa864a71d57900a96a189b3
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-04-28 15:39:50 +0530

    add user and video models

---
+++ src/models/users.models.js
@@ -0,0 +1,52 @@
+import {mongoose} from "mongoose";
+const userSchema =new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            required :true,
+            unique:true,
+            lowercase:true,
+            trim :true,
+            index : true,
+        },
+        email:{
+            type:String,
+            required :true,
+            unique:true,
+            lowercase:true,
+            trim :true,
+        },
+        fullname:{
+            type:String,
+            required :true,
+            index: true,
+        },
+        password:{
+            type:String,
+            required :[true,"Password is required"],
+            lowercase:true,
+        },
+        avtar:{
+            type:String,  //cloudinary url
+            required :true,
+            
+        },
+        coverImage:{
+            type:String,  //cloudinary url
+        },
+        watchHistory:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        refreshToken:{
+            type:String,
+            default :null,
+        },
+
+    },
+    {timestamps:true});
+
+
+export const User =mongoose.model("User",userSchema)
